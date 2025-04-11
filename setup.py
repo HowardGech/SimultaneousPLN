b552f2031ec84830da44605ef1bbfea70099b3c8
HowardGech <howardgech@gmail.com>
2025-04-11 00:03:31 -0400

    update python file

--- setup.py
+++ setup.py
@@ -40,12 +40,7 @@ ext_modules = [
         "SimultaneousPLN._SPLN",
         sources=["SimultaneousPLN/simultaneous_pln.pyx"],
         include_dirs=include_dirs,
-    ),
-    Extension(
-        "SimultaneousPLN._generation",
-        sources=["SimultaneousPLN/generation.pyx"],
-        include_dirs=include_dirs,
-    ),
+    )
 ]
 
 setup(
